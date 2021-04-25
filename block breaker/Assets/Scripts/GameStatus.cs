935eff4d388a62efc416c26b8f707e637ee825ec
Nikolay Sorokin <47783569+poncho-boncho@users.noreply.github.com>
2021-04-25 23:26:47 +0300

    added the display of score on the screen

--- block breaker/Assets/Scripts/GameStatus.cs
+++ block breaker/Assets/Scripts/GameStatus.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
 
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 40;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     [SerializeField] int currentScore = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = currentScore.ToString();
     }
 
     // Update is called once per frame
@@ -25,5 +27,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
